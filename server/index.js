78b02dfd114cb77d4b98e7f82161cea767bd9b2c
Samuel Pinheiro <44897109+devPinheiro@users.noreply.github.com>
2019-05-01 15:58:41 +0100

    ch(swagger): setup API documentation using Swagger (#10)
    - install swagger-ui-express
    - add config to index.js
    - add swagger.json file
    
    [Finishes #165596313]

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const swagger = require('swagger-ui-express');
+const swaggerDocument = require('../swagger');
 
 dotenv.config();
 const app = express();
@@ -7,6 +9,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({ message: 'Hello Books Deferral' });
 });
+
+/**
+ *  setup Swagger
+ */
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument, { explorer: true }));
 
 const { PORT } = process.env;
 app.listen(PORT);
