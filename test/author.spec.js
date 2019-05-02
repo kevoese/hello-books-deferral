5e335767c05631854a8758fd308a9d78b7f79f59
obi chinedu frank <frankieetchy@gmail.com>
2019-05-02 15:47:39 +0100

    feat(author) : implements user can add author  - ensures the  data sent is saved properly.
    165764173

--- test/author.spec.js
+++ test/author.spec.js
@@ -1,5 +1,26 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
+const server = require('../server/index');
+
+const { apiURL } = global;
+
 chai.use(chaiHttp);
 const { expect } = chai;
+
+describe('Add Author', () => {
+  it('it should be able successfully add an author', (done) => {
+    const data = {
+      name: 'Micheal Angelo',
+    };
+    chai.request(server)
+      .post(`${apiURL}/authors`).send(data)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res).to.be.a('object');
+        expect(res.body).to.have.property('status');
+        expect(res.body).to.have.property('body');
+        done();
+      });
+  });
+});
