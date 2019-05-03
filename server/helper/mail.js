aef06e8c6df298f3234307328635e1c6ef34ac09
Samuel Pinheiro <pinheirolaoluwa@gmail.com>
2019-05-03 04:12:31 +0100

    ft(mail): Setup a mail class and integrate sendgrid add test for edge cases install sendgrid package update .env.sample with api key create helper mail class implement chainable methods within mail class setup mail.config file for 3rd party mailing system [Finishes #165596316]

---
+++ server/helper/mail.js
@@ -0,0 +1,54 @@
+const SendGrid = require('@sendgrid/mail');
+const config = require('../../mail.config');
+
+/**
+ * Mail class for sending mail
+ * @chainable
+ */
+class Mail {
+  /**
+   * @param {string} subject
+   */
+  constructor(subject) {
+    this.payload = {
+      subject
+    };
+  }
+
+  /**
+   * get recipient email
+   * @param {array} email
+   * @returns {object} this
+   */
+  to(email) {
+    this.payload.email = email;
+    return this;
+  }
+
+  /**
+   * get mail data
+   * @param {string} data
+   * @returns {string} this
+   */
+  message(data) {
+    this.payload.message = data;
+    return this;
+  }
+
+  /**
+   * send email to user
+   * using SendGrid
+   * @returns {object} data
+   */
+  send() {
+    SendGrid.setApiKey(config.SendGrid.apiKey);
+    const msg = {
+      to: this.payload.email,
+      from: 'hello-books@andela.com',
+      subject: this.payload.subject,
+      text: this.payload.message
+    };
+    return SendGrid.send(msg);
+  }
+}
+module.exports = Mail;
