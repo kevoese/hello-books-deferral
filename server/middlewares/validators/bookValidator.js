be177f808a78c954ab45d82366e08a91e19c5992
Kati Frantz <bahdcoder@gmail.com>
2019-05-06 19:00:17 +0100

    feat(users): User can add books (#21)
    -Creates book.spec.js file
    - Adds test for possible scenario of user can add books
    - Removes idle test on index.js
    - Updates .env.sample file
    - Edits clients on knexfile.js and adds directory for migrations on test environment
    - Adds Controller for add books feature
    - Adds migrations for the books table
    - Adds schema to create books table
    - Adds endpoint for the user can add books feature
    
    [Delivers #165762957]

---
+++ server/middlewares/validators/bookValidator.js
@@ -0,0 +1,30 @@
+const { validateAll } = require("indicative");
+
+const addBook = (req, res, next) => {
+  const rules = {
+    title: "required|string",
+    coverType: "required|string",
+    description: "required|string",
+    isbn: "required|string",
+    publisher: "required|string",
+    year: "required|number"
+  };
+
+  const data = req.body;
+
+  const messages = {
+    required: "{{ field }} is required to create a book"
+  };
+
+  validateAll(data, rules, messages)
+    .then(() => {
+      next();
+    })
+    .catch(errors => {
+      // [{ field: 'title', message: 'title is required to create a book' }, { field: 'isbn', message: 'isbn is required to create a book' }]
+      //
+      res.status(422).jerror("ValidationFailed", errors);
+    });
+};
+
+module.exports = addBook;
