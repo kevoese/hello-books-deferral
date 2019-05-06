d849f3ffac33024c3e2edb5cb4a756c9e9d6b457
fantastic-genius <fantastigenius@yahoo.com>
2019-05-06 13:14:14 +0100

    #165596315 User Receives Token on Sucessful Registration (#19)
    * feat(authController, auth.spec, packaje.json) User should receive token on signup
    
    - added jsowebtoken to package.json
    - implemented token generation on successful signup in authController.signUp
    - updated signup route test to check for token
    - removed if-else statement in authController.signUp
    
    [Delivers #165596315]

--- server/middlewares/controllers/authController.js
+++ server/middlewares/controllers/authController.js
@@ -1,16 +1,28 @@
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
 
 const signUp = async (req, res) => {
   const { firstName, lastName, password, email } = req.body;
 
-  await User.query().insert({
+  const user = await User.query().insert({
     firstName,
     lastName,
     password,
     email
   });
 
-  res.status(201).jsend({ message: "User registered" });
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "12h"
+    }
+  );
+
+  res.status(201).jsend({
+    message: "User registered",
+    token
+  });
 };
 
 module.exports = { signUp };
