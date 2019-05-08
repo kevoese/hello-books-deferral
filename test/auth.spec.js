a6dc22eb2dc19f39e1dcee060b8ee17192c532d2
Esegbona Kelvin <45713801+kevoese@users.noreply.github.com>
2019-05-08 18:44:45 +0100

    feat(verification): Implement email account verification (#25)
    - Include verificationlink to welcome message
    - Create endpoint "/api/v1/auth/verify/<verificationCode>" to handle verification
    - Add test codes for the above endpoint
    
    [Delivers #165879964]

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -1,6 +1,7 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { server, databaseConnection } = require("../server/index");
+const User = require("../server/models/User");
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -63,6 +64,65 @@ describe("AUTH API ENDPOINTS", () => {
       expect(res.body.code).to.equal("ValidationFailed");
       expect(res.body.message[0]).to.have.property("message");
       expect(res.body.message[0].message).to.include("email must be unique");
+    });
+  });
+  
+  describe("POST ADD AN AUTHOR api/v1/authors", () => {
+    it("should addd a new author", done => {
+      data = {
+        name: "John Doe"
+      };
+      chai
+        .request(server)
+        .post("/api/v1/authors")
+        .send(data)
+        .end((err, res) => {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property("status");
+          done();
+        });
+    });
+
+    it("should not add an author if name field is empty", async () => {
+      data.name = "";
+      const res = await chai
+        .request(server)
+        .post("/api/v1/authors")
+        .send(data);
+      expect(res).to.have.status(422);
+      expect(res.body.status).to.include("error");
+      expect(res.body.code).to.equal("ValidationFailed");
+      expect(res.body.message).to.be.an("array");
+      expect(res.body.message[0].message).to.include(
+        "name is required to create an author"
+      );
+    });
+
+    it("should not verify an email with a wrong verification code", async () => {
+      const res = await chai
+        .request(server)
+        .post("/api/v1/auth/verify/:thisistherandomstring")
+        .send();
+      expect(res.body.status).to.include("error");
+      expect(res).to.have.status(400);
+      expect(res.body.code).to.be.an("object");
+      expect(res.body.code.error).to.equal("Bad Request");
+    });
+
+    it("should verify an email with a good verification code", async () => {
+      const thisUser = await User.query().where("email", register.email);
+      const verificationCode = thisUser[0].email_confirm_code;
+      const res = await chai
+        .request(server)
+        .post(`/api/v1/auth/verify/${verificationCode}`)
+        .send();
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.property("status");
+      expect(res.body.status).to.include("success");
+      expect(res.body).to.have.property("data");
+      expect(res.body.data).to.be.an("object");
+      expect(res.body.data).to.have.property("message");
+      expect(res.body.data.message).to.include("User verified");
     });
   });
 });
