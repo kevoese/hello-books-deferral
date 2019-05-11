b2dabfb737d17fd71074233b6fdd724a8b34bf05
obi chinedu frank <frankieetchy@gmail.com>
2019-05-11 09:10:40 +0100

    feat(BookRequest): Users should be able to request for a book that's not in the library
     - add controller, model, route and validation for bookRequest
     - add migration book_request table
     - add test for feature
     - add get all book request route
    [Delivers #165596338]

--- server/middleware/auth/authenticate.js
+++ server/middleware/auth/authenticate.js
@@ -1,71 +1,71 @@
-import User from "@models/User";
-import jwt from "jsonwebtoken";
+import User from '@models/User';
+import jwt from 'jsonwebtoken';
 
 export const isAuthenticated = async (req, res, next) => {
-  const token = req.headers["x-access-token"];
-  if (!token) {
-    return res.status(400).jsend({
-      message: "Unauthenticated"
-    });
-  }
-  try {
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-    const { email } = decoded;
-    let user = await User.query()
-      .where("email", email)
-      .first();
-
-    if (!user) {
-      return res.status(400).jsend({
-        message: "Unauthenticated"
-      });
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(400).jsend({
+            message: 'Unauthenticated'
+        });
     }
+    try {
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        const { email } = decoded;
+        let user = await User.query()
+            .where('email', email)
+            .first();
 
-    req.user = { id: user.id, email: user.email, role: user.role };
-    next();
-  } catch (error) {
-    return res.status(500).jsend({
-      message: "Unauthenticated"
-    });
-  }
+        if (!user) {
+            return res.status(400).jsend({
+                message: 'Unauthenticated'
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(500).jsend({
+            message: 'Unauthenticated'
+        });
+    }
 };
 
 export const isAdmin = (req, res, next) => {
-  const { role } = req.user;
+    const { role } = req.user;
 
-  if (role !== "admin") {
-    return res.status(403).jsend({
-      message: "UnAuthorised"
-    });
-  }
-  next();
+    if (role !== 'admin') {
+        return res.status(403).jsend({
+            message: 'UnAuthorised'
+        });
+    }
+    next();
 };
 
 const isSuperAdmin = (req, res, next) => {
-  const { role } = req.user;
+    const { role } = req.user;
 
-  if (role !== "super_admin") {
-    return res.status(403).jsend({
-      message: "UnAuthorised"
-    });
-  }
-  next();
+    if (role !== 'super_admin') {
+        return res.status(403).jsend({
+            message: 'UnAuthorised'
+        });
+    }
+    next();
 };
 
 const isPatron = (req, res, next) => {
-  const { role } = req.user;
+    const { role } = req.user;
 
-  if (role !== "patron") {
-    return res.status(403).jsend({
-      message: "UnAuthorised"
-    });
-  }
-  next();
+    if (role !== 'patron') {
+        return res.status(403).jsend({
+            message: 'UnAuthorised'
+        });
+    }
+    next();
 };
 
 export default {
-  isAuthenticated,
-  isAdmin,
-  isSuperAdmin,
-  isPatron
+    isAuthenticated,
+    isAdmin,
+    isSuperAdmin,
+    isPatron
 };
