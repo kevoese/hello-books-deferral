a1f8ab59074b2803285e3d1eae8d4957d0f26fa7
Kati Frantz <bahdcoder@gmail.com>
2019-05-16 11:55:49 +0100

    chore(app): setup client (#41)

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from '@pages/Main';
+import '@client/styles/main.css';
+
+const app = document.getElementById('app');
+
+const render = () => {
+    ReactDOM.render(<Main />, app);
+};
+
+if (app) render();
+
+if (module.hot) module.hot.accept();
