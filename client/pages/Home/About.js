f31d2a3578946734935f761b2fdc79f8f973da2c
obi chinedu frank <30419322+FrankChinedu@users.noreply.github.com>
2019-05-16 17:44:08 +0100

    #166056839 Feature client side routing  (#42)
     - set up react router
    
    [Delivers #166056839]

---
+++ client/pages/Home/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About us</h1>
+            <p>
+                {' '}
+                What is Lorem Ipsum ? Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry.Lorem Ipsum has been the
+                industry 's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it t
+            </p>
+        </div>
+    );
+};
+
+export default About;
--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import About from './Home/About';
+import index from './Home/index';
+import Navbar from './Home/Navbar';
 
 export default function Main() {
-    return <React.Fragment>{/** render router here */}</React.Fragment>;
+    return (
+        <React.Fragment>
+            <BrowserRouter>
+                <Navbar />
+                <Route exact path="/" component={index} />
+                <Route path="/about" component={About} />
+            </BrowserRouter>
+        </React.Fragment>
+    );
 }
