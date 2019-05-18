dad22ef3fd8134133bce3988754c239610cb84ca
fantastic-genius <fantastigenius@yahoo.com>
2019-05-18 23:38:26 +0100

    feat(ResetPassword Main tailwind.config) reset password UI - designed user interface to request to reset password to new one - added custom css in tailwindcss config [Delivers #166083775]

---
+++ client/pages/Home/ResetPassword.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const ResetPassword = () => {
+    return (
+        <div className="absolute w-full h-full bg-gray-200">
+            <div className="absolute w-4/5 md:w-1/2 lg:w-2/5 inset-x-1/10 md:inset-x-1/4 lg:inset-x-3/10 inset-y-1/5 md:inset-y-1/5">
+                <h2 className="text-3xl mb-3 text-center capitalize">
+                    Reset Password
+                </h2>
+                <div className="bg-white rounded md:rounded-lg shadow md:shadow-md px-8 py-8 md:px-6 md:py-12 text-sm md:text-base leading-normal">
+                    <form>
+                        <div className="lg:mx-8">
+                            <label className="inline-block w-1/3 lg:w-2/5">
+                                New Password
+                            </label>
+                            <input
+                                className="bg-gray-200 w-3/5 lg:w-3/5 p-2 rounded-full inline"
+                                name="email"
+                                type="email"
+                                placeholder="Enter new password"
+                            />
+                        </div>
+                        <div className="lg:mx-8">
+                            <label className="inline-block w-1/3 lg:w-2/5">
+                                Confirm Password
+                            </label>
+                            <input
+                                className="bg-gray-200 w-3/5 lg:w-3/5 mt-4 p-2 rounded-full inline"
+                                name="email"
+                                type="email"
+                                placeholder="Enter password again"
+                            />
+                        </div>
+                        <input
+                            className="block bg-primary mx-auto px-5 py-2 md:px-6 rounded-full mt-4"
+                            type="submit"
+                            value="Submit"
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ResetPassword;
