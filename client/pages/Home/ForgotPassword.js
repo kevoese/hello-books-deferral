7b7bd6ae62eb7e1be5e4d2e2f893acd4dd34caea
fantastic-genius <fantastigenius@yahoo.com>
2019-05-18 23:38:26 +0100

    feat(ResetPassword Main tailwind.config) reset password UI - designed user interface to request to reset password to new one - added custom css in tailwindcss config - made some fixes [Delivers #166083775]

--- client/pages/Home/ForgotPassword.js
+++ client/pages/Home/ForgotPassword.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const ForgotPassword = () => {
     return (
-        <div className="absolute w-full h-full bg-gray-200">
+        <div
+            className="absolute w-full h-full font-raleway"
+            style={{ background: `url(/images/bg-12.png)` }}
+        >
             <div className="absolute w-3/5 md:w-1/2 lg:w-1/3 inset-1/5 md:inset-x-1/4 lg:inset-x-1/3 md:inset-y-1/5">
                 <h2 className="text-3xl mb-3 text-center capitalize">
                     Reset Password
@@ -12,14 +15,14 @@ const ForgotPassword = () => {
                         <div className="text-center">
                             <label className="inline">Email</label>
                             <input
-                                className="bg-gray-200 w-4/5 ml-2 sm:ml-3 p-2 rounded-full inline"
+                                className="bg-gray-200 w-4/5 ml-2 sm:ml-3 p-2 rounded-lg inline"
                                 name="email"
                                 type="email"
                                 placeholder="Enter your email"
                             />
                         </div>
                         <input
-                            className="block bg-primary mx-auto px-5 py-2 md:px-6 rounded-full mt-4"
+                            className="block text-white border border-blue-500 bg-blue-500 rounded-full shadow mx-auto px-5 py-2 md:px-6 rounded mt-4"
                             type="submit"
                             value="Reset"
                         />
