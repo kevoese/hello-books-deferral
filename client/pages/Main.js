8b898369e262c523c8715d15453181290aa2c29b
fantastic-genius <fantastigenius@yahoo.com>
2019-05-18 22:27:19 +0100

    feat(ForgotPassword Main tailwind.config) forgot password UI - designed user interface to request for password reset - added custom css in tailwindcss config [Delivers #166083775]

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,41 +1,19 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import About from '@home/About';
-import index from '@home/index';
-import Navbar from '@home/Navbar';
-import ForgotPassword from '@home/ForgotPassword';
-import ResetPassword from '@home/ResetPassword';
-=======
 import About from '@pages/About';
 import index from '@pages/index';
-=======
-import Home from '@pages/Home';
->>>>>>> #166056907 (#51)
 import Navbar from '@components/Navbar';
 import { BrowserRouter, Route } from 'react-router-dom';
->>>>>>> #166056907 homepage first sections (#44)
 
 export default function Main() {
     return (
         <React.Fragment>
-<<<<<<< HEAD
-            <BrowserRouter>
-                <Navbar />
-                <Route exact path="/" component={index} />
-                <Route path="/about" component={About} />
-                <Route path="/forgot" component={ForgotPassword} />
-                <Route path="/reset" component={ResetPassword} />
-            </BrowserRouter>
-=======
             <div className="md:pl-3 lg:pl-3 xl:pl-3 md:pr-3 lg:pr-3 xl:pr-3">
                 <BrowserRouter>
                     <Navbar />
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={index} />
+                    <Route path="/about" component={About} />
                 </BrowserRouter>
             </div>
->>>>>>> #166056907 homepage first sections (#44)
         </React.Fragment>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,9 @@ import { colors } from 'tailwindcss/defaultTheme';
 
 module.exports = {
     theme: {
-<<<<<<< HEAD
-        extend: {
-            colors: {
-                gray: {
-                    ...colors.gray,
-                    '200': '#f1f1f1'
-                },
-                primary: '#51BCF3'
-            }
-        },
-        inset: {
-            '1/3': '33.333%',
-            '1/4': '25%',
-            '1/5': '20%',
-            '1/10': '10%',
-            '3/10': '30%'
-=======
         extend: {},
         fontFamily: {
             raleway: ['Raleway']
->>>>>>> #166056907 homepage first sections (#44)
         }
     },
     variants: {},
