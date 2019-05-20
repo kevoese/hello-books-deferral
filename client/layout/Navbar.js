3ebcb381192055946b3de1c67316f31d500ef472
Kati Frantz <bahdcoder@gmail.com>
2019-05-20 19:20:24 +0100

    #166056907 (#51)
    * chore(app): normalize staging (#40)
    
    *  ft(ladingpage): create landing page ui components
    install classnames package
    add Raleway Google Fonts
    add Navbar component
    [Finishes #166056907]

---
+++ client/layout/Navbar.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import classJoin from 'classnames';
+
+const Navbar = () => {
+    const [isVisible, changeVisibility] = useState(false);
+    const classToggle = classJoin({
+        block: isVisible,
+        hidden: !isVisible
+    });
+
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-white-500 pt-5 p-3">
+            <div className="flex items-center flex-shrink-0 text-dark mr-6">
+                <Link to="/">
+                    <img className="w-full " src="/images/hello_books.png" />
+                </Link>
+            </div>
+            <div className="block lg:hidden">
+                <button
+                    className="flex items-center px-3 py-2 border navToggler rounded text-dark-200 border-dark-500 hover:text-dark hover:border-dark"
+                    onClick={() => changeVisibility(!isVisible)}
+                >
+                    <svg
+                        className="fill-current h-3 w-3"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </button>
+            </div>
+            <div
+                id="navLink"
+                className={`${classToggle} w-full flex-grow lg:visible xl:visible lg:flex lg:items-center lg:w-auto`}
+            >
+                <div className="text-sm text-center m-auto pt-8 lg:pt-1 xl:pt-1">
+                    <Link
+                        className="font-raleway block font-semibold md:text-lg lg:text-lg xl:text-lg text-sm mr-8 mt-4 lg:inline-block lg:mt-0 text-dark-200 hover:text-dark mr-4"
+                        to="/"
+                    >
+                        Home
+                        <div className="m-auto lg:text-left xl:text-left">
+                            <svg
+                                width="40"
+                                height="2"
+                                className="m-auto"
+                                viewBox="0 0 40 2"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <line
+                                    y1="1"
+                                    x2="26"
+                                    y2="1"
+                                    stroke="#009DEE"
+                                    strokeWidth="2"
+                                />
+                                <line
+                                    x1="30"
+                                    y1="1"
+                                    x2="40"
+                                    y2="1"
+                                    stroke="#009DEE"
+                                    strokeWidth="2"
+                                />
+                            </svg>
+                        </div>
+                    </Link>
+                    <Link
+                        className="font-raleway block font-semibold  md:text-lg lg:text-lg xl:text-lg mr-8 mt-4 lg:inline-block lg:mt-0 text-dark-200 hover:text-dark mr-4"
+                        to="/"
+                    >
+                        Library Catalogue
+                    </Link>
+                    <Link
+                        className="font-raleway block font-semibold  md:text-lg lg:text-lg xl:text-lg mr-8 mt-4 lg:inline-block lg:mt-0 text-dark-200 hover:text-dark"
+                        to="/About"
+                    >
+                        About
+                    </Link>
+                    <Link
+                        className="font-raleway block font-semibold  md:text-lg lg:text-lg xl:text-lg mr-8 mt-4 lg:inline-block lg:mt-0 text-dark-200 hover:text-dark"
+                        to="/"
+                    >
+                        FAQs
+                    </Link>
+                </div>
+                <div className="text-sm text-center lg:mr-5 xl:mr-5">
+                    <Link
+                        className="font-raleway inline-block text-sm px-4 py-2 pr-10 pl-10 border-400 rounded-full text-blue-500 border border-blue-500 hover:border-transparent hover:bg-white mt-4 shadow lg:mt-0"
+                        to="/"
+                    >
+                        Sign In
+                    </Link>
+                </div>
+                <div className="text-sm text-center">
+                    <Link
+                        className="font-raleway inline-block text-sm px-4 py-2 pr-10 pl-10 border-500 rounded-full text-white border border-blue-500 bg-blue-500 hover:border-white mt-4 shadow lg:mt-0"
+                        to="/"
+                    >
+                        Register
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import About from '@pages/About';
-import index from '@pages/index';
+import Home from '@pages/Home';
 import Navbar from '@components/Navbar';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -10,8 +9,7 @@ export default function Main() {
             <div className="md:pl-3 lg:pl-3 xl:pl-3 md:pr-3 lg:pr-3 xl:pr-3">
                 <BrowserRouter>
                     <Navbar />
-                    <Route exact path="/" component={index} />
-                    <Route path="/about" component={About} />
+                    <Route exact path="/" component={Home} />
                 </BrowserRouter>
             </div>
         </React.Fragment>
