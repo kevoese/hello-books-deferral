3526d1ea1b6593cf69d6c4592f2caca3e689e254
Esegbona Kelvin <45713801+kevoese@users.noreply.github.com>
2019-05-22 22:01:48 +0100

    #166093221: Implement user registration page (#46)
    * feat(user) - Implement user sign up page
    [ Delivers #166093221 ]
    
    * feat(user) - Implement user sign up page[ Delivers #166093221 ]

---
+++ client/validations/Auth.js
@@ -0,0 +1,16 @@
+import * as Yup from 'yup';
+
+export const RegisterValidator = Yup.object().shape({
+    email: Yup.string()
+        .email()
+        .required('Required'),
+    firstName: Yup.string().required(),
+    lastName: Yup.string().required(),
+    password: Yup.string()
+        .required('Password is required')
+        .min(8, 'Password must be greater than 8 characters'),
+    passwordConfirmation: Yup.string().oneOf(
+        [Yup.ref('password'), null],
+        'Passwords must match'
+    )
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,25 @@ module.exports = {
         },
         extend: {
             colors: {
-                'light-gray': '#F1F1F1',
-                'blue-1100': '#51bcf3'
+                gray: {
+                    '250': '#F1F1F1',
+                    '550': 'rgba(36, 36, 36, 0.7)'
+                },
+                blue: {
+                    '550': '#51bcf3'
+                }
             }
+        },
+        boxShadow: {
+            custom: '0px 0px 25px rgba(0, 0, 0, 0.15)'
+        },
+        borderRadius: {
+            '10': '10px',
+            '54': '1.5rem',
+            full: '9999px'
+        },
+        maxWidth: {
+            custom: '40rem'
         }
     },
     variants: {},
