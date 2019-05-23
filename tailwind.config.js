cf3458a717949166e459e7dbba70ff4ea75aaecd
Samuel Pinheiro <pinheirolaoluwa@gmail.com>
2019-05-23 09:49:49 +0100

    merge conflict fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
         extend: {
             colors: {
                 gray: {
+                    '50': '#FDFDFD',
                     '250': '#F1F1F1',
                     '550': 'rgba(36, 36, 36, 0.7)'
                 },
                 blue: {
-                    '550': '#51bcf3'
+                    '550': '#51bcf3',
+                    '450': '#009DEE'
                 }
             }
         },
